80f41c77c31f20afe424465eb9651fe5409a0e46
root <root@sysmgr01.agss.net>
2018-11-17 15:19:31 -0500

    modified:   provider.tf

--- vmware/provider.tf
+++ vmware/provider.tf
@@ -1,7 +1,7 @@
 provider "vsphere" {
   user           = "Administrator@vsphere"
-  password       = "vM0tion14#"
-  vsphere_server = "resvc-as-01v.email.comcast.net"
+  password       = "password"
+  vsphere_server = "vecnter-server"
 
   # If you have a self-signed cert
   allow_unverified_ssl = true
